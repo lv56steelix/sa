d0e817325fa5b67202edb6fad42838c4c677ea42
Sebastian <72105591+lv56steelix@users.noreply.github.com>
2020-12-12 18:44:24 -0600

    Adding search folder

---
+++ oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+define(function () {
+return ["how_to_chain_stitch.html@@@How To Chain Stitch@@@A quick tutorial on the first crochet stitch you should learn!...","tools_you_need.html@@@Tools You Need@@@A short list of tools you need to start crocheting!...","why_crochet.html@@@Why Crochet?@@@Crochet is the process of turning yarn, thread, and other materials into textiles using a single hook..."];
+});
---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,10 @@
+define(function() {var keywords=[{w:"How",p:["p0"]},{w:"To",p:["p0"]},{w:"Chain",p:["p0"]},{w:"Stitch",p:["p0"]},{w:"Tools",p:["p1"]},{w:"You",p:["p1"]},{w:"Need",p:["p1"]},{w:"Why",p:["p2"]},{w:"Crochet?",p:["p2"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6];
+ph["p2"]=[7, 8];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
